90f7b7f22f0c93809d2c35028dc6e43d920747bb
lexgp <lexgp@yandex.ru>
2025-04-21 12:57:29 +0300

    Пока скрыл графики

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -4,8 +4,6 @@ import AnalyticsTransactions from '@/views/dashboard/AnalyticsTransactions.vue'
 import DeliveryCard from '@/views/dashboard/DeliveryCard.vue'
 import ReviewsWidget from '@/views/dashboard/ReviewsWidget.vue'
 
-import CustomerChurn from '@/views/dashboard/charts/CustomerChurn.vue'
-import SalesForecast from '@/views/dashboard/charts/SalesForecast.vue'
 
 const totalProfit = {
   title: 'Total Profit',
@@ -29,7 +27,7 @@ const newProject = {
 <template>
   <VRow class="match-height">
 
-    <!-- <VCol cols="12" md="12">
+  <!-- <VCol cols="12" md="12">
       <VAlert color="success"
         title="Что готово на 14.04.2025:"
         icon="ri-alert-fill"
@@ -69,43 +67,43 @@ const newProject = {
           Всесторонний когортный анализ
         </VBtn>, 
 
-        <VBtn variant="text"
+          <VBtn variant="text"
+            color="success"
+            prepend-icon="ri-chat-check-fill"  
+            to="/analyzes/nlp"
+          >
+            Извлечение признаков из отзывов с помощью LLM
+          </VBtn>, 
+
+          <VBtn variant="text"
           color="success"
           prepend-icon="ri-chat-check-fill"  
-          to="/analyzes/nlp"
-        >
-          Извлечение признаков из отзывов с помощью LLM
-        </VBtn>, 
+            to="/analyzes/k-means"
+          >
+            K means + доп.продажи
+          </VBtn>, 
 
-        <VBtn variant="text"
-          color="success"
-          prepend-icon="ri-chat-check-fill"  
-          to="/analyzes/k-means"
-        >
-          K means + доп.продажи
-        </VBtn>, 
+          <VBtn variant="text"
+            color="primary"
+            prepend-icon="ri-chat-check-fill"  
+            to="/ml-models/time-machine"
+          >
+            Первая версия нашей первой предсказательной модели
+          </VBtn>, 
 
-        <VBtn variant="text"
-          color="primary"
-          prepend-icon="ri-chat-check-fill"  
-          to="/ml-models/time-machine"
-        >
-          Первая версия нашей первой предсказательной модели
-        </VBtn>, 
-
-      </VAlert>
-    </VCol> -->
+        </VAlert>
+      </VCol> -->
 
 
-    <!-- 3 -->
+  <!-- 3 -->
 
-    <VCol cols="12" md="6">
-      <DeliveryCard />
-    </VCol>
+  <VCol cols="12" md="6">
+    <DeliveryCard />
+  </VCol>
 
     <!-- <VCol cols="12" md="4">
-      <AnalyticsTotalEarning />
-    </VCol> -->
+        <AnalyticsTotalEarning />
+      </VCol> -->
 
     <VCol cols="12" md="6">
       <ReviewsWidget />
@@ -121,13 +119,13 @@ const newProject = {
       <AnalyticsTransactions />
     </VCol>
 
-    <VCol cols="12" md="6">
-      <CustomerChurn />
-    </VCol>
+    <!-- <VCol cols="12" md="6">
+        <CustomerChurn />
+      </VCol>
 
-    <VCol cols="12" md="6">
-      <SalesForecast />
-    </VCol>
+      <VCol cols="12" md="6">
+        <SalesForecast />
+      </VCol> -->
 
   </VRow>
 </template>
